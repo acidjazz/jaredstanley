e0b522df46735e68a1de535c487b779c10ef9764
acidjazz <acidjazz@gmail.com>
2019-06-21 02:05:20 -0500

    🚧 menu and format cleanup

--- components/header/NavBar.vue
+++ components/header/NavBar.vue
@@ -1,11 +1,10 @@
 <template lang="pug">
-    .navbar.absolute.top-0.inset-x-0.bg-blue-300.flex
+    .navbar.fixed.z-50.top-0.inset-x-0.flex.justify-end
         nuxt-link.py-2.px-4(
             v-for="item in menu"
             :to="item.route"
             :key="item.route"
-            :class="{'bg-red-300': item.routeName === $route.name}"
-            ) {{ item.name }}
+            :class="{'bg-gray-300': item.routeName === $route.name}") {{ item.name }}
 </template>
 
 <script>
@@ -16,19 +15,19 @@ export default {
             active: 'home',
             menu: [
                 {
-                name:'home',
-                route:"/",
-                routeName:"index"
+                  name:'home',
+                  route:"/",
+                  routeName:"index"
                 },
                 {
-                name:'about',
-                route:"/about",
-                routeName:"about"
+                  name:'about',
+                  route:"/about",
+                  routeName:"about"
                 },
                 {
-                name:'contact',
-                route:"/contact",
-                routeName:"contact"
+                  name:'contact',
+                  route:"/contact",
+                  routeName:"contact"
                 }
             ]
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 #jaredstanley
-  //NavBar
+  NavBar
   nuxt
 </template>
 
